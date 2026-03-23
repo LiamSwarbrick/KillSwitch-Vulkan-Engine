f1b858bd967fb6ae1836985a096d17579550369b
Davidlxy <107847811+Davidlxy@users.noreply.github.com>
2026-03-23 08:22:18 +0000

    Add files via upload

---
+++ src/core/resource_manager.h
@@ -0,0 +1,106 @@
+#ifndef ENGINE_RESOURCE_MANAGER_H
+#define ENGINE_RESOURCE_MANAGER_H
+
+#include "core.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define RESOURCE_NAME_MAX_LEN 64
+#define RESOURCE_PATH_MAX_LEN 260
+#define RESOURCE_BACKEND_RID_NONE 0xFFFFFFFFu
+
+typedef u32 ResourceHandle;
+
+typedef enum ResourceType
+{
+    RESOURCE_TYPE_UNKNOWN = 0,
+    RESOURCE_TYPE_BINARY,
+    RESOURCE_TYPE_SHADER_BYTECODE,
+    RESOURCE_TYPE_TEXTURE,
+    RESOURCE_TYPE_MESH,
+    RESOURCE_TYPE_MATERIAL,
+    RESOURCE_TYPE_AUDIO,
+    RESOURCE_TYPE_FONT,
+    RESOURCE_TYPE_SKELETON,
+    RESOURCE_TYPE_COLLISION,
+    RESOURCE_TYPE_WORLD
+}
+ResourceType;
+
+typedef enum ResourceResidency
+{
+    RESOURCE_RESIDENCY_BOOT = 0,
+    RESOURCE_RESIDENCY_PERMANENT,
+    RESOURCE_RESIDENCY_TRANSIENT
+}
+ResourceResidency;
+
+typedef enum ResourceLoadState
+{
+    RESOURCE_LOAD_STATE_UNREGISTERED = 0,
+    RESOURCE_LOAD_STATE_REGISTERED,
+    RESOURCE_LOAD_STATE_LOADED,
+    RESOURCE_LOAD_STATE_FAILED
+}
+ResourceLoadState;
+
+typedef struct ResourceRecord
+{
+    ResourceHandle handle;
+    ResourceType type;
+    ResourceResidency residency;
+    ResourceLoadState state;
+
+    u32 generation;
+    u32 backend_rid;
+    u64 byte_size;
+    void* loaded_data;
+
+    char logical_name[RESOURCE_NAME_MAX_LEN];
+    char source_path[RESOURCE_PATH_MAX_LEN];
+}
+ResourceRecord;
+
+typedef struct ResourceManagerCreateInfo
+{
+    const char* debug_name;
+    u32 initial_capacity;
+}
+ResourceManagerCreateInfo;
+
+typedef struct ResourceManager
+{
+    ThreadAllocTracker tt;
+    ResourceRecord* records;
+    u32 record_count;
+    u32 record_capacity;
+}
+ResourceManager;
+
+ResourceManager ResourceManager_Create(ResourceManagerCreateInfo create_info);
+void ResourceManager_Destroy(ResourceManager* manager);
+
+ResourceHandle ResourceManager_FindByName(const ResourceManager* manager, const char* logical_name);
+ResourceHandle ResourceManager_FindByPath(const ResourceManager* manager, const char* source_path);
+
+ResourceHandle ResourceManager_RequestFile(
+    ResourceManager* manager,
+    ResourceType type,
+    ResourceResidency residency,
+    const char* logical_name,
+    const char* source_path
+);
+
+const ResourceRecord* ResourceManager_Get(const ResourceManager* manager, ResourceHandle handle);
+void ResourceManager_Unload(ResourceManager* manager, ResourceHandle handle);
+void ResourceManager_UnloadByResidency(ResourceManager* manager, ResourceResidency residency);
+void ResourceManager_SetBackendRID(ResourceManager* manager, ResourceHandle handle, u32 backend_rid);
+void ResourceManager_LogSummary(const ResourceManager* manager);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
