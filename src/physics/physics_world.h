4fd4e79b45cce786495bec2a5a2388d3fc8ea18c
Jimmy <jimmiya@tuta.io>
2026-03-25 18:29:56 +0000

    commit so i can move to blender2render

--- src/physics/physics_world.h
+++ src/physics/physics_world.h
@@ -8,9 +8,11 @@
 // At a fixed step half the expected frame time, say for 60Hz we use a fixed 1/120 step
 // Update() :
 //      Apply Forces -- (gravity for now) (and forever)
+//      -- Input velocities should be calculated before using this (on the input manager or whatever the #%!@) 
 //      Constraints (not for now)
 //      Broadphase -- coarse collision check update
-//      Narrowphase -- generates contacts
-//      
-//      
+//      Narrowphase -- checks collisions
+//      Generate contacts -- (explore islands in the future)
+//      Solve contacts --
+//		Integrate positions --
 //      
