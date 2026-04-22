7aa4263a3c3f8a49dd5810eca04279e35ee1c6ca
Nansong Yue <sc20ny@leeds.ac.uk>
2026-04-23 00:18:06 +0100

    New actions needed can be added in input_action

---
+++ src/core/input_actions.h
@@ -0,0 +1,42 @@
+#ifndef CORE_INPUT_ACTIONS_H
+#define CORE_INPUT_ACTIONS_H
+
+#include <stdint.h>
+
+
+//  To add a new action:
+//    1. Add a row here
+//    2. Add default bindings in set_default_bindings() in input.cpp
+
+#define INPUT_ACTIONS_LIST \
+    /* Movement */         \
+    X(MOVE_FORWARD,  "move_forward")  \
+    X(MOVE_BACKWARD, "move_backward") \
+    X(MOVE_LEFT,     "move_left")     \
+    X(MOVE_RIGHT,    "move_right")    \
+    X(MOVE_UP,       "move_up")       \
+    X(MOVE_DOWN,     "move_down")     \
+    X(SPRINT,        "sprint")        \
+    X(JUMP,          "jump")          \
+    /* Camera */           \
+    X(CAMERA_UP,     "camera_up")     \
+    X(CAMERA_DOWN,   "camera_down")   \
+    X(CAMERA_LEFT,   "camera_left")   \
+    X(CAMERA_RIGHT,  "camera_right")  \
+    /* Gameplay */         \
+    X(INTERACT,      "interact")      \
+    X(ATTACK,        "attack")        \
+    /* UI / System */      \
+    X(PAUSE,         "pause")         \
+    X(DEBUG_TOGGLE,  "debug_toggle")
+
+// Generate the enum from the list above
+enum InputAction : uint32_t
+{
+#define X(suffix, name) ACTION_##suffix,
+    INPUT_ACTIONS_LIST
+#undef X
+    ACTION_COUNT  // must always be last
+};
+
+#endif // CORE_INPUT_ACTIONS_H
