c38d393ad097401ab011eca253b3225c61214382
Jimmy <jimmiya@tuta.io>
2026-05-15 16:20:39 +0100

    Changed the physics step to be 60fps and max steps to 2.

--- src/physics/physics_settings.h
+++ src/physics/physics_settings.h
@@ -10,8 +10,8 @@ struct PhysicsSettings
 	float sleepBiasRate = 0.2; // To turn to sleepBias = std::pow(sleepBiasRate, dt);
 
 	// World Up vector and Gravity depend on PhysicsWorld, so they stay there
-	float fixedStepDuration = 1.0f / 120.0f; // 0.0083333333f
-	float maxIterationSteps = 4; // Should prob be 8 or more
+	float fixedStepDuration = 1.0f / 60.0f; // 0.0083333333f
+	float maxIterationSteps = 2; // Should prob be 8 or more
 	// At 30 fps we would reach the maximum number of 4 iteration steps.
 	
 };
